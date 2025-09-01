fc2bcb8ddcbad0889709f03bee3ba898d264a22d
Riya Raj <rrajj7674@gmail.com>
2025-09-01 17:03:41 +0000

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { swagger } from "@elysiajs/swagger";
+import { Elysia, t } from "elysia";
+import { createPost, getPost, getPosts, updatePost } from "./handlers";
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => {
+    return { message: "Welcome to the API" };
+  })
+  .get("/post", () => getPosts())
+  .get("/post/:id", ({ params: { id } }) => getPost(id), {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+  })
+  .post("/", ({ body }) => createPost(body), {
+    body: t.Object({
+      title: t.String({
+        minLength: 3,
+        maxLength: 50,
+      }),
+      content: t.String({
+        minLength: 3,
+        maxLength: 50,
+      }),
+    }),
+  })
+  .put("/post/:id", ({ params: { id }, body }) => updatePost(id, body), {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+    body: t.Object(
+      {
+        title: t.Optional(
+          t.String({
+            minLength: 3,
+            maxLength: 50,
+          })
+        ),
+        content: t.Optional(
+          t.String({
+            minLength: 3,
+            maxLength: 50,
+          })
+        ),
+      },
+      {
+        minProperties: 1,
+      }
+    ),
+  })
+  .listen(4000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
